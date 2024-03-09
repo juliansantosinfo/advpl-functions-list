03fc36c0f9d5c359a261dc149497b7afa6b3f8f2
Julian de Almeida Santos <julian.santos.info@gmail.com>
2024-03-09 19:59:09 -0300

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,52 @@
+![icon](images/icon.png)
+
+# advpl-functions-list
+
+A extensão *Function List* é uma ferramenta poderosa projetada para desenvolvedores que trabalham com Visual Studio Code. Esta extensão oferece uma maneira conveniente de listar e explorar funções presentes no arquivo em edição diretamente na barra lateral do explorador de arquivos.
+
+Com a *Function List*, os desenvolvedores podem organizar e visualizar facilmente as funções presentes no código-fonte, separadas em duas categorias principais: Funções Estáticas e Funções do Usuário.
+
+## Recursos Principais
+
+- **Navegação Simplificada:** Navegue rapidamente pelas funções do arquivo atualmente em edição diretamente na barra lateral do explorador de arquivos.
+  
+- **Organização por Categoria:** As funções estão organizadas em duas categorias distintas: *Static Functions* e *User Functions*, facilitando a localização e a compreensão do código.
+  
+- **Atualização Automática:** A lista de funções é atualizada automaticamente conforme você edita o arquivo, garantindo que você sempre tenha acesso às últimas alterações.
+
+## Como Usar
+
+Basta abrir um arquivo de código-fonte no Visual Studio Code e visualizar a barra lateral do explorador de arquivos. Você encontrará dois menus: *Static Functions* e *User Functions*, cada um contendo as respectivas funções encontradas no arquivo em edição.
+
+## Amostra
+
+![gif-sample](images/sample.gif)
+
+## Requisitos
+
+Não há requisitos ou dependências específicas para esta extensão. Ela se integra perfeitamente ao seu ambiente do Visual Studio Code.
+
+## Configurações da Extensão
+
+advplParametersList.regex.GetMV: Define expressao regex para busca das chamadas a funcao GetMV
+advplParametersList.regex.GetNewPar: Define expressao regex para busca das chamadas a funcao GetNewPar
+advplParametersList.regex.SuperGetMV: Define expressao regex para busca das chamadas a funcao SuperGetMV
+
+## Contribua
+
+Você pode contribuir com o desenvolvimento desta extensão de várias maneiras:
+
+1. Reporte problemas e sugira novos recursos na [página de issues do GitHub](https://github.com/juliansantosinfo/advpl-functions-list/issues).
+2. Faça um fork do repositório, faça suas alterações e envie um pull request.
+3. Ajude a melhorar a documentação.
+4. Compartilhe esta extensão com outros desenvolvedores.
+
+## Problemas Conhecidos
+
+Atualmente não há problemas conhecidos com esta extensão. Se encontrar algum problema, por favor, reporte na [página de issues do GitHub](https://github.com/juliansantosinfo/advpl-functions-list/issues).
+
+## Notas de Lançamento
+
+
+- **1.0.0:** Lançamento inicial da extensão **advpl-parameters-list**. Adiciona suporte para listar e explorar funções estáticas e funções do usuário diretamente na barra lateral do explorador de arquivos.
+
---
+++ extension.js
@@ -0,0 +1,335 @@
+const vscode = require("vscode");
+
+/**
+ * This method is called when your extension is activated
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  console.log("Extensão AdvPL Functions List foi ativada.");
+  vscode.window.showInformationMessage(
+    "Extensão AdvPL Functions List foi ativada."
+  );
+
+  // Criar a barra lateral e registrar a classe como provedor de dados
+  const sideBarMenuStaticFunctions = vscode.window.createTreeView(
+    "sideBarMenuStaticFunctions",
+    {
+      treeDataProvider: new SideBarMenuStaticFunctionsProvider(),
+    }
+  );
+
+  // Criar a barra lateral e registrar a classe como provedor de dados
+  const sideBarMenuUserFunctions = vscode.window.createTreeView(
+    "sideBarMenuUserFunctions",
+    {
+      treeDataProvider: new SideBarMenuUserFunctionsProvider(),
+    }
+  );
+
+  context.subscriptions.push(sideBarMenuStaticFunctions);
+  context.subscriptions.push(sideBarMenuUserFunctions);
+
+  // Adicionar um manipulador de eventos para abrir o arquivo na linha correspondente
+  vscode.commands.registerCommand(
+    "advplFunctionsList.sideBarMenu.openFile",
+    (lineNumber) => {
+      const activeEditor = vscode.window.activeTextEditor;
+      if (activeEditor) {
+        const newPosition = new vscode.Position(lineNumber - 1, 0); // Linhas são baseadas em zero
+        const newSelection = new vscode.Selection(newPosition, newPosition);
+        activeEditor.selection = newSelection;
+        activeEditor.revealRange(
+          new vscode.Range(newPosition, newPosition),
+          vscode.TextEditorRevealType.InCenter
+        );
+      }
+    }
+  );
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {
+  console.log("Extensão AdvPL Functions List foi desativada.");
+  vscode.window.showInformationMessage(
+    "Extensão AdvPL Functions List foi desativada."
+  );
+}
+
+class SideBarMenuStaticFunctionsProvider {
+  constructor() {
+    // EventEmitter para notificar quando os dados da árvore mudam
+    this._onDidChangeTreeData = new vscode.EventEmitter();
+    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+
+    // Registrar observadores para eventos de alteração no editor de texto
+    vscode.workspace.onDidChangeTextDocument(() => {
+      this.refresh(); // Atualizar os dados da árvore quando o documento for modificado
+    });
+
+    vscode.window.onDidChangeActiveTextEditor(() => {
+      this.refresh(); // Atualizar os dados da árvore quando o editor ativo mudar
+    });
+
+    vscode.workspace.onDidCloseTextDocument(() => {
+      this.refresh(); // Atualizar os dados da árvore quando um documento for fechado
+    });
+  }
+
+  refresh() {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element) {
+    let elementLabel;
+    let elementLineNumber;
+    let collapsibleState;
+    let commandSideBarMenuOpenFile;
+
+    elementLabel = element.label;
+    elementLineNumber = element.lineNumber;
+
+    if (element.parameters && element.parameters.length > 0) {
+      collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+    } else {
+      collapsibleState = vscode.TreeItemCollapsibleState.None;
+    }
+
+    commandSideBarMenuOpenFile = {
+      title: "Abrir arquivo",
+      command: "advplFunctionsList.sideBarMenu.openFile",
+      arguments: [elementLineNumber],
+    };
+
+    return {
+      label: elementLabel,
+      collapsibleState: collapsibleState,
+      command: commandSideBarMenuOpenFile,
+    };
+  }
+
+  async getChildren(element) {
+    if (element) {
+      let parametersList = [];
+      if (element.parameters) {
+        element.parameters.forEach((element) => {
+          parametersList.push({
+            label: element,
+          });
+        });
+      }
+      return parametersList;
+    } else {
+      let staticFunctionsList = [];
+      getStaticFunctionsInActiveEditor().forEach((element) => {
+        staticFunctionsList.push(element);
+      });
+      return staticFunctionsList;
+    }
+  }
+}
+
+class SideBarMenuUserFunctionsProvider {
+  constructor() {
+    // EventEmitter para notificar quando os dados da árvore mudam
+    this._onDidChangeTreeData = new vscode.EventEmitter();
+    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+
+    // Registrar observadores para eventos de alteração no editor de texto
+    vscode.workspace.onDidChangeTextDocument(() => {
+      this.refresh(); // Atualizar os dados da árvore quando o documento for modificado
+    });
+
+    vscode.window.onDidChangeActiveTextEditor(() => {
+      this.refresh(); // Atualizar os dados da árvore quando o editor ativo mudar
+    });
+
+    vscode.workspace.onDidCloseTextDocument(() => {
+      this.refresh(); // Atualizar os dados da árvore quando um documento for fechado
+    });
+  }
+
+  refresh() {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element) {
+    let elementLabel;
+    let elementLineNumber;
+    let collapsibleState;
+    let commandSideBarMenuOpenFile;
+
+    elementLabel = element.label;
+    elementLineNumber = element.lineNumber;
+
+    if (element.parameters && element.parameters.length > 0) {
+      collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
+    } else {
+      collapsibleState = vscode.TreeItemCollapsibleState.None;
+    }
+
+    commandSideBarMenuOpenFile = {
+      title: "Abrir arquivo",
+      command: "advplFunctionsList.sideBarMenu.openFile",
+      arguments: [elementLineNumber],
+    };
+
+    return {
+      label: elementLabel,
+      collapsibleState: collapsibleState,
+      command: commandSideBarMenuOpenFile,
+    };
+  }
+
+  async getChildren(element) {
+    if (element) {
+      let parametersList = [];
+      if (element.parameters) {
+        element.parameters.forEach((element) => {
+          parametersList.push({
+            label: element,
+          });
+        });
+      }
+      return parametersList;
+    } else {
+      let userFunctionsList = [];
+      getUserFunctionsInActiveEditor().forEach((element) => {
+        userFunctionsList.push(element);
+      });
+      return userFunctionsList;
+    }
+  }
+}
+
+function getStaticFunctionsInActiveEditor() {
+  let activeEditor;
+  let textEditor;
+  let staticFunctionsList;
+  let extensionSettings;
+  let regexExpression;
+  let matches;
+  let elementLabel;
+  let elementParametersList;
+
+  activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    return [];
+  }
+
+  extensionSettings = vscode.workspace.getConfiguration("advplFunctionsList");
+
+  textEditor = activeEditor.document.getText();
+  if (textEditor.length == 0) {
+    return [];
+  }
+
+  staticFunctionsList = [];
+  regexExpression = extensionSettings.regex.staticfunctions;
+  regexExpression = RegExp(regexExpression, "gi");
+
+  while ((matches = regexExpression.exec(textEditor)) !== null) {
+    elementLabel = matches[1];
+    elementParametersList = [];
+
+    matches[2].split(",").forEach((element) => {
+      element = element.trim();
+      if (element.length > 0) {
+        if (element.search(" as ") > 0) {
+          element = element.split(" as ").join(" : ");
+        } else {
+          if (element.startsWith("c")) {
+            element = `${element} : character`;
+          } else if (element.startsWith("n")) {
+            element = `${element} : numeric`;
+          } else if (element.startsWith("a")) {
+            element = `${element} : array`;
+          } else if (element.startsWith("d")) {
+            element = `${element} : date`;
+          } else if (element.startsWith("o")) {
+            element = `${element} : object`;
+          } else if (element.startsWith("j")) {
+            element = `${element} : json`;
+          }
+        }
+        elementParametersList.push(element.trim());
+      }
+    });
+
+    staticFunctionsList.push({
+      label: elementLabel,
+      parameters: elementParametersList,
+      lineNumber: activeEditor.document.positionAt(matches.index).line + 1,
+    });
+  }
+
+  return staticFunctionsList;
+}
+
+function getUserFunctionsInActiveEditor() {
+  let activeEditor;
+  let textEditor;
+  let userFunctionsList;
+  let extensionSettings;
+  let regexExpression;
+  let matches;
+  let elementLabel;
+  let elementParametersList;
+
+  activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    return [];
+  }
+
+  extensionSettings = vscode.workspace.getConfiguration("advplFunctionsList");
+
+  textEditor = activeEditor.document.getText();
+  if (textEditor.length == 0) {
+    return [];
+  }
+
+  userFunctionsList = [];
+  regexExpression = extensionSettings.regex.userfunctions;
+  regexExpression = RegExp(regexExpression, "gi");
+
+  while ((matches = regexExpression.exec(textEditor)) !== null) {
+    elementLabel = matches[1];
+    elementParametersList = [];
+
+    matches[2].split(",").forEach((element) => {
+      element = element.trim();
+      if (element.trim().length > 0) {
+        if (element.search(" as ") > 0) {
+          element = element.split(" as ").join(" : ");
+        } else {
+          if (element.startsWith("c")) {
+            element = `${element} : character`;
+          } else if (element.startsWith("n")) {
+            element = `${element} : numeric`;
+          } else if (element.startsWith("a")) {
+            element = `${element} : array`;
+          } else if (element.startsWith("d")) {
+            element = `${element} : date`;
+          } else if (element.startsWith("o")) {
+            element = `${element} : object`;
+          } else if (element.startsWith("j")) {
+            element = `${element} : json`;
+          }
+        }
+        elementParametersList.push(element.trim());
+      }
+    });
+
+    userFunctionsList.push({
+      label: elementLabel,
+      parameters: elementParametersList,
+      lineNumber: activeEditor.document.positionAt(matches.index).line + 1,
+    });
+  }
+
+  return userFunctionsList;
+}
+
+module.exports = {
+  activate,
+  deactivate,
+};
